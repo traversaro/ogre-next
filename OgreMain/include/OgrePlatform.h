c7e225f9c72f04121942287bcc34a9ce79d24dd0
Silvio Traversaro <silvio@traversaro.it>
2022-11-26 00:37:42 +0100

    Update OgrePlatform.h

--- OgreMain/include/OgrePlatform.h
+++ OgreMain/include/OgrePlatform.h
@@ -64,7 +64,7 @@ THE SOFTWARE.
 /* Find CPU type */
 #if defined(__i386__) || defined(__x86_64__) || defined(_M_IX86) || defined(_M_X64) || defined(_M_AMD64) || defined(__e2k__)
 #   define OGRE_CPU OGRE_CPU_X86
-#elif defined(__ppc__) || defined(__ppc64__) || defined(_M_PPC)
+#elif defined(__PPC__) || defined(__PPC64__) || defined(_M_PPC)
 #   define OGRE_CPU OGRE_CPU_PPC
 #elif defined(__arm__) || defined(__arm64__) || defined(__aarch64__) || defined(_M_ARM) || defined(_M_ARM64) 
 #   define OGRE_CPU OGRE_CPU_ARM
@@ -76,7 +76,7 @@ THE SOFTWARE.
 
 /* Find the arch type */
 #if defined(__x86_64__) || defined(_M_X64) || defined(_M_X64) || defined(_M_AMD64) \
- || defined(__ppc64__) \
+ || defined(__PPC64__) \
  || defined(__arm64__) || defined(__aarch64__) || defined(_M_ARM64) \
  || defined(__mips64) || defined(__mips64_) \
  || defined(__alpha__) || defined(__ia64__) || defined(__e2k__) || defined(__s390__) || defined(__s390x__)
